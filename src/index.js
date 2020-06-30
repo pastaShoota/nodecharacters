952d27696e9e47c2997687e3c14eec6889f5a11c
Alex del Var <alex.del.var@gmail.com>
2020-06-30 06:11:25 +0200

    tp-2 - starter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-console.log('TP-1');
+console.log('TP-2');
 
 const fs = require('fs').promises;
 
-console.time();
-Promise.all([0, 1, 2]
-  .map(index => './characters-' + index + '.json')
+Promise.all([0, 1]
+  .map(index => './src/assets/characters-' + index + '.json')
   .map(filePath => fs.readFile(filePath, 'utf-8')))
   .then(fileContents => {
     fileContents
       .map(fileContent => JSON.parse(fileContent))
       .forEach(data => console.log(data))
-    console.timeEnd();
   });
