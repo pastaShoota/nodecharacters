a22f8e4437343e609bb071f8f5a517e323b64f4f
Alex del Var <alex.del.var@gmail.com>
2020-07-01 08:05:16 +0200

    tp-5 - solution

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,10 @@ const appRouter = require('./app.router');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Incoming request on path ' + req.path + ' with parameters ' + JSON.stringify(req.query));
+  next();
+})
 app.use(appRouter);
 
 module.exports = app;
