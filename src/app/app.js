1ff061be6db06939c7a8af3ed499d79421a25b3d
Alex del Var <alex.del.var@gmail.com>
2020-07-02 09:31:16 +0200

    tp-10 - solution (1)

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const helmet = require('helmet');
 const appRouter = require('./app.router');
 const errorMiddleware = require('./common/error/error.middleware');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Incoming request on path ' + req.path + ' with parameters ' + JSON.stringify(req.query));
-  next();
-})
-app.use(express.json());
-app.use(appRouter);
-app.use(errorMiddleware);
+app
+  .use((req, res, next) => {
+    console.log('Incoming request on path ' + req.path + ' with parameters ' + JSON.stringify(req.query));
+    next();
+  })
+  .use(helmet())
+  .use(express.json())
+  .use(appRouter)
+  .use(errorMiddleware);
 
 module.exports = app;
