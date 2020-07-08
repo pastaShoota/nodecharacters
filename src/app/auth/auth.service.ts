173fc77255560dd61bf411b3bb01de7687c752eb
Thierry Chevalier <thierry.chevalier@lyra-network.com>
2020-07-08 17:26:59 +0200

    my last commit

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,24 +1,12 @@
 import jwt from 'jsonwebtoken';
 import { AppError, AppErrorType } from '../common/error/app-error';
+import { sign as jwtSign, verify as jwtVerify } from 'jwt-promise';
 
 const secret = 'my-seceret';
 
-export const sign = <T extends Object>(payload: T) => new Promise<string>((resolve, reject) => {
-  jwt.sign(payload, secret, (error, token) => {
-    if (error) {
-      reject(new AppError(AppErrorType.AUTHENTICATION_ERROR, error));
-    } else {
-      resolve(token);
-    }
-  });
-});
+export const sign = <T extends Object>(payload: T) => jwtSign(payload, secret)
+    .catch(error => new AppError(AppErrorType.AUTHENTICATION_ERROR, error));
 
-export const verify = <T>(token) => new Promise<T>((resolve, reject) => {
-  jwt.verify(token, secret, (error, payload) => {
-    if (error) {
-      reject(new AppError(AppErrorType.AUTHORIZATION_ERROR, error));
-    } else {
-      resolve(payload);
-    }
-  });
-});
+export const verify = <T>(token) => jwtVerify(token, secret)
+    .catch(error => new AppError(AppErrorType.AUTHORIZATION_ERROR, error));
+
